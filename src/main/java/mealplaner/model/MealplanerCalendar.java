afebeda1c7b5925df95e7c114687f843b79b44e2
Martin Idel <martin.idel@googlemail.com>
2017-07-29 15:03:54 +0200

    Use simpler method to substract dates and add tests

--- src/main/java/mealplaner/model/MealplanerCalendar.java
+++ src/main/java/mealplaner/model/MealplanerCalendar.java
@@ -4,61 +4,52 @@ import java.io.Serializable;
 import java.util.Calendar;
 
 public class MealplanerCalendar implements Serializable {
+	private static final int MILLISECONDS_PER_DAY = 86400000;
+	private static final int LAST_HOUR = 23;
+	private static final int LAST_MINUTE_OR_SECOND = 59;
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Calendar date;
-	
+
 	public MealplanerCalendar() {
 		date = Calendar.getInstance();
 	}
-	
+
 	public Calendar getCalendar() {
 		return date;
 	}
-	
+
 	public void setCalendar(Calendar calendar) {
 		date = calendar;
 	}
-	
+
 	public void setDate(int year, int month, int day) {
-		date.set(year,month,day);
-	}	
-	
+		date.set(year, month, day);
+	}
+
 	public int updateCalendar() {
 		Calendar newCal = Calendar.getInstance();
 		int daysSince = getDaysPassedTo(newCal);
 		setCalendar(newCal);
 		return daysSince;
 	}
-	
-	public int getDaysPassedTo(Calendar newCal){
-		int dayOld = date.get(Calendar.DAY_OF_YEAR);	
-		int yearOld = date.get(Calendar.YEAR);
-		int dayNow = newCal.get(Calendar.DAY_OF_YEAR);	
-		int yearNow = newCal.get(Calendar.YEAR);
-		
-		int daysBetweenOldAndNow = 0;
-		if(newDateLiesInTheFutureOfOldDate(dayOld, yearOld, dayNow, yearNow)){
-			daysBetweenOldAndNow += addCompleteYears(yearOld, yearNow);
-			daysBetweenOldAndNow += addOrSubstractFurtherDays(dayOld, dayNow); 
-		}
-		return daysBetweenOldAndNow;
+
+	public int getDaysPassedTo(Calendar newCal) {
+		return getDaysBetween(date, newCal);
 	}
 
-	private boolean newDateLiesInTheFutureOfOldDate(int dayOld, int yearOld, int dayNow, int yearNow) {
-		return dayNow>=dayOld && yearNow>=yearOld || dayNow<dayOld && yearNow>yearOld;
-	}
-
-	private int addCompleteYears(int yearOld, int yearNow) {
-		int daysInBetweenYearOldAndYearNow = 0;
-		for(int i=yearOld; i<yearNow; i++){
-			daysInBetweenYearOldAndYearNow += (i%4)==0 ? 366 : 365;
+	public static int getDaysBetween(Calendar oldCal, Calendar newCal) {
+		long daysSince = 0;
+		if (newCal.after(oldCal)) {
+			Calendar newCalWithDateAtTheEndOfDay = Calendar.getInstance();
+			newCalWithDateAtTheEndOfDay.set(newCal.get(Calendar.YEAR),
+					newCal.get(Calendar.MONTH),
+					newCal.get(Calendar.DAY_OF_MONTH),
+					LAST_HOUR, LAST_MINUTE_OR_SECOND, LAST_MINUTE_OR_SECOND);
+			daysSince = (newCalWithDateAtTheEndOfDay.getTimeInMillis() - oldCal.getTimeInMillis())
+					/ MILLISECONDS_PER_DAY;
 		}
-		return daysInBetweenYearOldAndYearNow;
-	}
-	
-	private int addOrSubstractFurtherDays(int dayOld, int dayNow) {
-		return dayNow-dayOld;
+		return (int) daysSince;
 	}
 }
--- src/test/java/mealplaner/model/MealplanerCalendarTest.java
+++ src/test/java/mealplaner/model/MealplanerCalendarTest.java
@@ -1,6 +1,7 @@
 package mealplaner.model;
 
-import static org.junit.Assert.*;
+import static mealplaner.model.MealplanerCalendar.getDaysBetween;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 
@@ -8,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MealplanerCalendarTest {
-	
+
 	private MealplanerCalendar sut;
 
 	@Before
@@ -24,58 +25,105 @@ public class MealplanerCalendarTest {
 		assertEquals(1, sut.getCalendar().get(Calendar.DAY_OF_YEAR));
 		assertEquals(2015, sut.getCalendar().get(Calendar.YEAR));
 	}
-	
+
 	@Test
 	public void getDaysPassedToWithSomeDateInThePast() {
 		sut.setDate(2017, 0, 1);
 		Calendar compareToCalendar = getCustomCalendar(2015, 0, 1);
-		
+
 		int daysPassed = sut.getDaysPassedTo(compareToCalendar);
-		
+
 		assertEquals(0, daysPassed);
 	}
-	
+
 	@Test
 	public void getDaysPassedToSameYear() {
 		sut.setDate(2016, 0, 1);
 		Calendar compareToCalendar = getCustomCalendar(2016, 5, 7);
-		
+
 		int daysPassed = sut.getDaysPassedTo(compareToCalendar);
-		
-		assertEquals(30+29+31+30+31+7, daysPassed);
+
+		assertEquals(30 + 29 + 31 + 30 + 31 + 7, daysPassed);
 	}
-	
+
 	@Test
 	public void getDaysPassedToSameYearNoLeapYear() {
 		sut.setDate(2015, 0, 1);
 		Calendar compareToCalendar = getCustomCalendar(2015, 5, 7);
-		
+
 		int daysPassed = sut.getDaysPassedTo(compareToCalendar);
-		
-		assertEquals(30+28+31+30+31+7, daysPassed);
+
+		assertEquals(30 + 28 + 31 + 30 + 31 + 7, daysPassed);
 	}
-	
+
 	@Test
 	public void getDaysPassedToLessThanAYear() {
 		sut.setDate(2015, 9, 5);
 		Calendar compareToCalendar = getCustomCalendar(2016, 1, 7);
-		
+
 		int daysPassed = sut.getDaysPassedTo(compareToCalendar);
-		
-		assertEquals(26+30+31+31+7, daysPassed);
+
+		assertEquals(26 + 30 + 31 + 31 + 7, daysPassed);
 	}
-	
+
 	@Test
 	public void getDaysPassedToMoreThanAYear() {
 		sut.setDate(2014, 0, 1);
 		Calendar compareToCalendar = getCustomCalendar(2016, 5, 7);
-		
+
 		int daysPassed = sut.getDaysPassedTo(compareToCalendar);
-		
-		assertEquals(364+365+31+29+31+30+31+7, daysPassed);
+
+		assertEquals(364 + 365 + 31 + 29 + 31 + 30 + 31 + 7, daysPassed);
 	}
-	
-	
+
+	@Test
+	public void correctNumberOfDaysForDaysAppart() {
+		Calendar oldCal = Calendar.getInstance();
+		Calendar newCal = Calendar.getInstance();
+		oldCal.set(30, 10, 5, 20, 0, 0);
+		newCal.set(30, 10, 7, 20, 0, 0);
+
+		int daysPassed = getDaysBetween(oldCal, newCal);
+
+		assertEquals(2, daysPassed);
+	}
+
+	@Test
+	public void correctNumberOfDaysIfOldLiesInTheFutureAppart() {
+		Calendar oldCal = Calendar.getInstance();
+		Calendar newCal = Calendar.getInstance();
+		oldCal.set(31, 10, 5, 20, 0, 0);
+		newCal.set(30, 10, 7, 20, 0, 0);
+
+		int daysPassed = getDaysBetween(oldCal, newCal);
+
+		assertEquals(0, daysPassed);
+	}
+
+	@Test
+	public void correctNumberOfDaysForMonthsAndYearsAppart() {
+		Calendar oldCal = Calendar.getInstance();
+		Calendar newCal = Calendar.getInstance();
+		oldCal.set(30, 10, 5, 20, 0, 0);
+		newCal.set(32, 11, 7, 20, 0, 0);
+
+		int daysPassed = getDaysBetween(oldCal, newCal);
+
+		assertEquals(763, daysPassed);
+	}
+
+	@Test
+	public void correctNumberOfDaysForDaysAppartButNotMillis() {
+		Calendar oldCal = Calendar.getInstance();
+		Calendar newCal = Calendar.getInstance();
+		oldCal.set(30, 10, 5, 20, 0, 0);
+		newCal.set(30, 10, 6, 3, 0, 0);
+
+		int daysPassed = getDaysBetween(oldCal, newCal);
+
+		assertEquals(1, daysPassed);
+	}
+
 	private Calendar getCustomCalendar(int year, int month, int day) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(year, month, day);
