8c3e9c9f7cce8541bb9239d2ea24588fe457c61b
Martin Idel <martin.idel@googlemail.com>
2018-03-03 21:53:13 +0100

    Add test for ingredients

--- src/test/java/guittests/MenuItemsTest.java
+++ src/test/java/guittests/MenuItemsTest.java
@@ -1,13 +1,21 @@
 package guittests;
 
+import static java.nio.file.Files.readAllBytes;
 import static java.util.Optional.of;
 import static mealplaner.commons.NonnegativeInteger.nonNegative;
 import static mealplaner.model.Meal.createMeal;
+import static mealplaner.recipes.model.Ingredient.ingredient;
+import static mealplaner.recipes.model.IngredientType.SPICE;
+import static mealplaner.recipes.model.Measure.NONE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static testcommons.CommonFunctions.getRecipe1;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.swing.fixture.DialogFixture;
 import org.junit.Test;
 
 import guittests.helpers.AssertJMealplanerTestCase;
@@ -16,6 +24,7 @@ import mealplaner.model.enums.CookingPreference;
 import mealplaner.model.enums.CookingTime;
 import mealplaner.model.enums.ObligatoryUtensil;
 import mealplaner.model.enums.Sidedish;
+import mealplaner.recipes.model.Ingredient;
 
 public class MenuItemsTest extends AssertJMealplanerTestCase {
 
@@ -28,5 +37,25 @@ public class MenuItemsTest extends AssertJMealplanerTestCase {
     meals.add(meal1);
     windowHelpers.compareDatabaseInTable(meals);
     window.close();
+  }
+
+  @Test
+  public void createIngredient() throws IOException {
+    Ingredient ingredient = ingredient("Test4", SPICE, NONE);
+
+    window.menuItem("MenuFile").click();
+    window.menuItem("MenuItemInsertIngredients").click();
+    DialogFixture enterIngredientDialog = window.dialog();
+    enterIngredientDialog.textBox("InputFieldNonemptyTextIngredientName")
+        .setText(ingredient.getName());
+    enterIngredientDialog.comboBox("InputFieldComboBoxIngredientType")
+        .selectItem(ingredient.getType().toString());
+    enterIngredientDialog.comboBox("InputFieldComboBoxIngredientMeasure")
+        .selectItem(ingredient.getMeasure().toString());
+    enterIngredientDialog.button("ButtonPanelIngredientsInput0").click();
+    enterIngredientDialog.button("ButtonPanelIngredientsInput1").click();
+
+    assertThat(new String(readAllBytes(Paths.get(DESTINATION_INGREDIENT_FILE_PATH))))
+        .isEqualTo(new String(readAllBytes(Paths.get("src/test/resources/ingredientsAdded.xml"))));
   }
 }
--- src/test/java/guittests/helpers/AssertJMealplanerTestCase.java
+++ src/test/java/guittests/helpers/AssertJMealplanerTestCase.java
@@ -1,14 +1,20 @@
 package guittests.helpers;
 
 import static guittests.helpers.GuiMethods.create;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static mealplaner.commons.BundleStore.BUNDLES;
 import static mealplaner.io.IngredientProviderIoGui.loadIngredientProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.swing.JFrame;
 
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Assert;
 
 import mealplaner.MealplanerData;
 import mealplaner.gui.MainGui;
@@ -19,6 +25,19 @@ import mealplaner.recipes.provider.IngredientProvider;
 public class AssertJMealplanerTestCase extends AssertJSwingJUnitTestCase {
   protected FrameFixture window;
   protected GuiMethods windowHelpers;
+  protected static final String DESTINATION_FILE_PATH = "src/test/resources/saveTemp.xml";
+  protected static final String DESTINATION_INGREDIENT_FILE_PATH = "src/test/resources/ingredientsTemp.xml";
+  private String originFilePath;
+  private String originIngredientFilePath;
+
+  public AssertJMealplanerTestCase() {
+    this("src/test/resources/save.xml", "src/test/resources/ingredients.xml");
+  }
+
+  public AssertJMealplanerTestCase(String originFilePath, String originIngredientFilePath) {
+    this.originFilePath = originFilePath;
+    this.originIngredientFilePath = originIngredientFilePath;
+  }
 
   @Override
   protected void onSetUp() {
@@ -28,13 +47,44 @@ public class AssertJMealplanerTestCase extends AssertJSwingJUnitTestCase {
     windowHelpers = create(window);
   }
 
+  @Override
+  protected void onTearDown() {
+    try {
+      File ingredientFile = new File(DESTINATION_INGREDIENT_FILE_PATH);
+      Files.delete(ingredientFile.toPath());
+      File file = new File(DESTINATION_FILE_PATH);
+      Files.delete(file.toPath());
+    } catch (IOException ioex) {
+      Assert.fail("One of the files used as save files does not exist");
+    }
+  }
+
   private MainGui createMainApplication() {
-    IngredientProvider ingredientProvider = loadIngredientProvider(
-        "src/test/resources/ingredients.xml");
-    JFrame mainFrame = new JFrame(BUNDLES.message("mainFrameTitle"));
-    FileIoGui fileIoGui = new FileIoGui(mainFrame, "src/test/resources/save.xml");
-    MealplanerData data = new MealplanerData();
-    DialogFactory dialogFactory = new DialogFactory(mainFrame);
-    return new MainGui(mainFrame, data, ingredientProvider, dialogFactory, fileIoGui);
+    try {
+      IngredientProvider ingredientProvider = loadIngredientProvider(getIngredientsPath());
+      JFrame mainFrame = new JFrame(BUNDLES.message("mainFrameTitle"));
+      FileIoGui fileIoGui = new FileIoGui(mainFrame, useFilePath());
+      MealplanerData data = new MealplanerData();
+      DialogFactory dialogFactory = new DialogFactory(mainFrame);
+      return new MainGui(mainFrame, data, ingredientProvider, dialogFactory, fileIoGui);
+    } catch (IOException exception) {
+      Assert.fail("One of the files to use as save files does not exist");
+      return null;
+    }
+  }
+
+  protected String getIngredientsPath() throws IOException {
+    return copyFile(originIngredientFilePath, DESTINATION_INGREDIENT_FILE_PATH);
+  }
+
+  protected String useFilePath() throws IOException {
+    return copyFile(originFilePath, DESTINATION_FILE_PATH);
+  }
+
+  private String copyFile(String filename, String destinationPath) throws IOException {
+    File originalFile = new File(filename);
+    File temporaryFile = new File(destinationPath);
+    Files.copy(originalFile.toPath(), temporaryFile.toPath(), REPLACE_EXISTING);
+    return temporaryFile.toPath().toString();
   }
 }
