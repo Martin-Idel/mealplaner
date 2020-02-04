91cc4b0fc1b4d47aef62ecc05949c1b74b4e2399
Martin Idel <martin.idel@googlemail.com>
2020-02-04 11:56:59 +0100

    Improve secondary measure dialog with renderers

--- mealplaner-gui-commons/src/main/java/mealplaner/commons/gui/tables/cells/FlexibleClassRenderer.java
+++ mealplaner-gui-commons/src/main/java/mealplaner/commons/gui/tables/cells/FlexibleClassRenderer.java
@@ -4,17 +4,23 @@ package mealplaner.commons.gui.tables.cells;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.util.Optional;
 import java.util.function.Function;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class FlexibleClassRenderer extends DefaultTableCellRenderer {
   private transient Function<Object, String> getStringRepresentation;
-  private final Object defaultObject;
+  private final transient Optional<Object> defaultObject;
 
   public FlexibleClassRenderer(Function<Object, String> getStringRepresentation, Object defaultObject) {
     this.getStringRepresentation = getStringRepresentation;
-    this.defaultObject = defaultObject;
+    this.defaultObject = Optional.of(defaultObject);
+  }
+
+  public FlexibleClassRenderer(Function<Object, String> getStringRepresentation) {
+    this.getStringRepresentation = getStringRepresentation;
+    this.defaultObject = Optional.empty();
   }
 
   protected void setValue(Object value) {
@@ -25,7 +31,7 @@ public class FlexibleClassRenderer extends DefaultTableCellRenderer {
   public Component getTableCellRendererComponent(
       JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
     var component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-    if (defaultObject.equals(value)) {
+    if (defaultObject.isPresent() && defaultObject.equals(value)) {
       component.setForeground(Color.GRAY);
     }
     return component;
--- mealplaner-gui/src/main/java/mealplaner/gui/dialogs/ingredients/MeasureTable.java
+++ mealplaner-gui/src/main/java/mealplaner/gui/dialogs/ingredients/MeasureTable.java
@@ -7,7 +7,6 @@ import static mealplaner.commons.NonnegativeFraction.ONE;
 import static mealplaner.commons.gui.tables.FlexibleTableBuilder.createNewTable;
 import static mealplaner.commons.gui.tables.TableColumnBuilder.withContent;
 import static mealplaner.commons.gui.tables.TableColumnBuilder.withNonnegativeFractionContent;
-import static mealplaner.model.recipes.Measure.GRAM;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +18,7 @@ import javax.swing.JComboBox;
 import mealplaner.commons.NonnegativeFraction;
 import mealplaner.commons.Pair;
 import mealplaner.commons.gui.tables.Table;
+import mealplaner.commons.gui.tables.cells.FlexibleClassRenderer;
 import mealplaner.model.recipes.Measure;
 
 public final class MeasureTable {
@@ -35,18 +35,36 @@ public final class MeasureTable {
             .setValueToOrderedImmutableList(measures, (pair, measure) -> Pair.of(measure, pair.right))
             .getValueFromOrderedList(measures, pair -> pair.left)
             .setPreferredSize(50)
-            .overwriteTableCellEditor(new DefaultCellEditor(
-                new JComboBox<>(new Vector<>(removePrimary(primaryMeasure)))))
+            .overwriteTableCellEditor(defineCellEditorWithAllButPrimaryMeasure(primaryMeasure))
+            .overwriteTableCellRenderer(defineMeasureFromRenderer())
             .build())
         .addColumn(withNonnegativeFractionContent()
             .isEditable()
             .withColumnName(BUNDLES.message("secondaryMeasureConversionFactorColumn"))
             .setValueToOrderedImmutableList(measures, (pair, fraction) -> Pair.of(pair.left, fraction))
             .getValueFromOrderedList(measures, pair -> pair.right)
+            .overwriteTableCellRenderer(defineMeasureToRenderer(primaryMeasure))
             .build())
-        .addDefaultRowToUnderlyingModel(() -> measures.add(Pair.of(GRAM, ONE)))
+        .addDefaultRowToUnderlyingModel(() -> measures.add(Pair.of(removePrimary(primaryMeasure).get(0), ONE)))
         .deleteRowsOnDelete(row -> measures.remove((int) row))
         .buildDynamicTable();
+  }
+
+  private static FlexibleClassRenderer defineMeasureFromRenderer() {
+    return new FlexibleClassRenderer(
+        object -> (object instanceof Measure) ? "1 " + object.toString() : object.toString());
+  }
+
+  private static FlexibleClassRenderer defineMeasureToRenderer(Measure primaryMeasure) {
+    return new FlexibleClassRenderer(
+        object -> (object instanceof NonnegativeFraction)
+            ? object.toString() + " " + primaryMeasure.toString()
+            : object.toString());
+  }
+
+  private static DefaultCellEditor defineCellEditorWithAllButPrimaryMeasure(Measure primaryMeasure) {
+    return new DefaultCellEditor(
+        new JComboBox<>(new Vector<>(removePrimary(primaryMeasure))));
   }
 
   public static List<Measure> removePrimary(Measure primaryMeasure) {
